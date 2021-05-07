6e68919f10739440105fc3ee9653b5aaf7bb9ccd
yulinwuzhou <73929034+yulinwuzhou@users.noreply.github.com>
2021-05-07 17:09:49 +0800

    Update xmly_speed.py

--- xmly_speed.py
+++ xmly_speed.py
@@ -55,10 +55,10 @@ if "XMLY_SPEED_COOKIE" in os.environ:
 ###################################################
 # 可选项
 # 自定义设备命名,非必须 ;devices=["iPhone7P","huawei"];与cookiesList对应
-devices = ["iPhone 7"]
+devices = ["iPhone XS"]
 notify_time = 19                            # 通知时间,24小时制,默认19
 XMLY_ACCUMULATE_TIME = 1                    # 希望刷时长的,此处置1,默认打开;关闭置0
-UserAgent = "ting_v2.1.9_c5(CFNetwork, iOS 14.4.1, iPhone9,1)"
+UserAgent = "Mozilla/5.0 (iPhone; CPU iPhone OS 14_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 iting/2.1.9 kdtunion_iting/1.0 iting(main)/2.1.9/ios_1)"
 # 非iOS设备的需要的自行修改,自己抓包 与cookie形式类似
 
 
